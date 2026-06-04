7074f4cfbe0fff7106def327b6286b7a3e89dbb7
vanillacrepes <daveplayzminecraft@gmail.com>
2026-06-04 15:51:47 +0800

    added routes and auth

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+require('dotenv').config();
+
+console.log(process.env.API_KEY)
+
+const requireApiKey = require('./middleware/auth');
+app.use(requireApiKey);
+
 const port = 3000;
 
 //routes here
--- routes/water.js
+++ routes/water.js
@@ -2,8 +2,7 @@ const db = require('../database'); // gets the same database connection as datab
 const express = require('express');
 const router = express.Router();
 
-// imma be so fr I watched youtube for this
-
+// my beloved helpers I love helpers
 const prepared = {
   getUser: db.prepare('SELECT * FROM users WHERE user_id = ?'),
   insertUser: db.prepare('INSERT INTO users (user_id, created_at) VALUES (?, ?)'),
@@ -16,35 +15,86 @@ const prepared = {
     UPDATE water
     SET last_drank = ?, drinks_today = ?, last_reset = ?
     WHERE user_id = ?
+  `),
+  updateActive: db.prepare(`
+    UPDATE water
+    SET active = ?
+    WHERE user_id = ?
   `)
 };
 
-// helper for daily reset
 function isNewDay(lastReset) {
   const toDay = (ms) => Math.floor(ms / (24*60*60*1000));
   return toDay(Date.now()) > toDay(lastReset);
 }
 
-// POST /:userId/drank
-router.post('/:userId/drank', (req, res) => {
-  const { userId } = req.params;
-  const now = Date.now();
-
+function ensureUserDataExists(userId, now) {
   const user = prepared.getUser.get(userId);
-  if (!user) prepared.insertUser.run(userId, now);
 
+  var returnState = 0
+
+  if (!user) {
+    prepared.insertUser.run(userId, now); 
+    returnState = 1;
+  }
+  
   const water = prepared.getWater.get(userId);
   if(!water) {
     prepared.insertWater.run(userId, now, now);
+    returnState = 2;
+  }
+
+  return returnState;
+}
+//helpers - end
+
+// GET /:userId
+router.get('/:userId', (req, res) => {
+  const { userId } = req.params; 
+  const now = Date.now();
+
+  const dataStatus = ensureUserDataExists(userId, now);
+
+  const userWaterData = prepared.getWater.get(userId);
+
+  res.json(userWaterData);
+})
+
+// POST /:userId/drank
+router.post('/:userId/drank', (req, res) => {
+  const { userId } = req.params; 
+  const now = Date.now();
+
+  const dataStatus = ensureUserDataExists(userId, now);
+
+  if(dataStatus == 2) {
     return res.json({message: `Started tracking ${userId}! :D`, drinks_today: 1});
   }
 
+  const water = prepared.getWater.get(userId);
   const drinks = isNewDay(water.last_reset) ? 1 : water.drinks_today + 1;
-  const resetDay = isNewDay(water.lastReset) ? now : water.last_reset;
+  const resetDay = isNewDay(water.last_reset) ? now : water.last_reset;
 
   prepared.updateDrank.run(now, drinks, resetDay, userId);
 
   res.json({ message: 'Drink logged!', drinks_today: drinks});
 });
+
+// PATCH /:userId/active
+router.patch('/:userId/active', (req, res) => {
+  const { userId } = req.params; 
+  const now = Date.now();
+
+  const dataStatus = ensureUserDataExists(userId, now);
+
+  const activeStatus = prepared.getWater.get(userId).active;
+
+  const newActiveStatus = activeStatus == 1 ? 0 : 1;
+  const message = activeStatus == 1 ? 'inactive' : 'active';
+
+  prepared.updateActive.run(newActiveStatus, userId);
+
+  res.json({ message: message, active: newActiveStatus});
+})
 
 module.exports = router;
